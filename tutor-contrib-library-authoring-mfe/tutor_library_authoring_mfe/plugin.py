06407b89ea85c61bfa12536a200081a6178fca00
Brian Smith <bsmith@tcril.org>
2022-12-21 10:16:58 -0500

    add init stuff from blockstore plugin, clean up/document plugin.py

--- tutor-contrib-library-authoring-mfe/tutor_library_authoring_mfe/plugin.py
+++ tutor-contrib-library-authoring-mfe/tutor_library_authoring_mfe/plugin.py
@@ -6,6 +6,76 @@ from tutor import hooks as tutor_hooks
 
 from .__about__ import __version__
 
+########################################
+# CONFIGURATION
+########################################
+
+tutor_hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [
+        ("LIBRARY_AUTHORING_MICROFRONTEND_VERSION", __version__),
+    ]
+)
+
+########################################
+# INITIALIZATION TASKS
+########################################
+
+MY_INIT_TASKS: list[tuple[str, tuple[str, ...]]] = [
+    ("cms", ("library_authoring_mfe", "jobs", "init", "cms.sh")),
+]
+
+
+# For each task added to MY_INIT_TASKS, we load the task template
+# and add it to the CLI_DO_INIT_TASKS filter, which tells Tutor to
+# run it as part of the `init` job.
+for service, template_path in MY_INIT_TASKS:
+    full_path: str = pkg_resources.resource_filename(
+        "tutorblockstore", os.path.join("templates", *template_path)
+    )
+    with open(full_path, encoding="utf-8") as init_task_file:
+        init_task: str = init_task_file.read()
+    tutor_hooks.Filters.CLI_DO_INIT_TASKS.add_item((service, init_task))
+
+
+
+########################################
+# TEMPLATE RENDERING
+########################################
+
+tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_items(
+    # Root paths for template files, relative to the project root.
+    [
+        pkg_resources.resource_filename("tutor_library_authoring_mfe", "templates"),
+    ]
+)
+
+########################################
+# PATCH LOADING
+########################################
+
+# For each file in tutor_library_authoring_mfe/patches,
+# apply a patch based on the file's name and contents.
+for path in glob(
+    os.path.join(
+        pkg_resources.resource_filename("tutor_library_authoring_mfe", "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+
+
+# ENDENDEND
+# ENDENDEND
+# ENDENDEND
+# ENDENDEND
+# ENDENDEND
+# ENDENDEND
+# ENDENDEND
+# ENDENDEND
+
+
 # # Enable the MFE in the CMS FEATURES configuration
 # hooks.Filters.ENV_PATCHES.add_item(("cms-env-features",
 #     "ENABLE_LIBRARY_AUTHORING_MICROFRONTEND: true"
@@ -67,18 +137,3 @@ from .__about__ import __version__
 #     });
 # }
 # """))
-
-# Configure plugin templates
-tutor_hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
-    pkg_resources.resource_filename("tutor_library_authoring_mfe", "templates")
-)
-
-# Load patches from files
-for path in glob(
-    os.path.join(
-        pkg_resources.resource_filename("tutor_library_authoring_mfe", "patches"),
-        "*",
-    )
-):
-    with open(path, encoding="utf-8") as patch_file:
-        tutor_hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
